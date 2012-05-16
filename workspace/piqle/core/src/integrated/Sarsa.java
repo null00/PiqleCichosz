6322ff69bd5a1118c713dd3227cbdc287f2ba559
Jaroslaw Szczesniak <null@student.agh.edu.pl>
2012-05-16 19:11:55 +0200

    [IDEA 1] Sarsa integration

---
+++ workspace/piqle/core/src/integrated/Sarsa.java
@@ -0,0 +1,23 @@
+package integrated;
+
+import algorithms.AbstractMemorySelector;
+import environment.IAction;
+import environment.IState;
+
+public class Sarsa extends AbstractMemorySelector {
+
+	private static final long serialVersionUID = 1L;
+
+	public Sarsa() {
+		
+	}
+	
+	public void learn(IState s1, IState s2, IAction a, double reward) {
+		
+	}
+	
+	public void showHistogram() {
+		
+	}
+	
+}
